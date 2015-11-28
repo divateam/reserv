d0efed414146fecf361ff3f925c7b7e8199d2658
lorizzimp <loriz.kiehiz@gmail.com>
2015-11-29 01:09:09 +0700

    Eclipse Add and First Commit

--- menu.php
+++ menu.php
@@ -3,8 +3,12 @@
 <head>
 <meta charset="utf-8">
 <title>Menu</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
-
 <body>
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
